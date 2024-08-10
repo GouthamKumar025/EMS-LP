194c6adc98ed611d22efcfcc58c7c284c8c5c6e5
Goutham Kumar S <113292942+GouthamKumar025@users.noreply.github.com>
2024-08-10 14:50:04 +0530

    Update Employee_manage.java

--- src/Employee_manage.java
+++ src/Employee_manage.java
@@ -4,7 +4,5 @@ import java.util.HashMap;
 public class Employee_manage {
     ArrayList<EmployeeDetails> employee_details = new ArrayList<>();
     HashMap<Integer,EmployeeDetails> employeeMap = new HashMap<>();
-
-
-    }
+}
 
